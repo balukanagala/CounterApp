0a600ea2a7c8faec553bb1d0bb5af23dcfc5d3ad
Ravi Kumar <ravinecw@gmail.com>
2023-12-05 10:08:59 +0530

    Modified code in js

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 var number=0;//global variable
 function count(){
-    number++;
-    document.getElementById("count").innerHTML = number;
+    document.getElementById("count").innerHTML = ++number;
 }
 function countback(){
-    number--;
-    document.getElementById("count").innerHTML = number;   
+    document.getElementById("count").innerHTML = --number;   
 }
 function reset(){
     number=0;
